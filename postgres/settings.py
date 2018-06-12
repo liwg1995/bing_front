ddb1d4a56779f63f08fec918d6b309542f0400aa
olei <i@olei.me>
2018-06-12 12:13:20 +0800

    update setting

--- postgres/settings.py
+++ postgres/settings.py
@@ -129,5 +129,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT='/var/www/postgres/static'
-STATICFILES_DIRS = (os.path.join(os.path.dirname(__file__),'../static/')),
+#STATIC_ROOT='/var/www/postgres/static'
+#STATICFILES_DIRS = (os.path.join(os.path.dirname(__file__),'../static/')),
